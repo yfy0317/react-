176ae9c94fc93a8473eef6d18ec3902ca9d7d92d
yangfeiyu5 <yangfeiyu5@jd.com>
2020-04-03 15:58:02 +0800

    fix: 我也改了

--- src/pages/business_pages/login/heart/index.js
+++ src/pages/business_pages/login/heart/index.js
@@ -13,7 +13,7 @@ export default class Login extends parentComponent {
   render() {
     return (
       <div>
-        <div onClick={() => this.ccc()}> 登录了</div>
+        <div onClick={() => this.ccc()}> 登录了333</div>
       </div>
     );
   }
