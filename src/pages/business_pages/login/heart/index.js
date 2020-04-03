65e0ed73625a0fb7568a0215c39c64af404efa19
yangfeiyu5 <yangfeiyu5@jd.com>
2020-04-03 16:44:16 +0800

    热热热

--- src/pages/business_pages/login/heart/index.js
+++ src/pages/business_pages/login/heart/index.js
@@ -13,7 +13,7 @@ export default class Login extends parentComponent {
   render() {
     return (
       <div>
-        <div onClick={() => this.ccc()}> 我改了333</div>
+        <div onClick={() => this.ccc()}> 44444</div>
       </div>
     );
   }
